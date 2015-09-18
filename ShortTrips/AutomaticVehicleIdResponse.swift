3da524e828e889dcb478fc9087d2a806db656419
devmonster423 <devmonster423@gmail.com>
2015-09-18 09:19:03 -0700

    network-layer cleanup: group model structs and make class methods reflect API

---
+++ ShortTrips/AutomaticVehicleIdResponse.swift
@@ -0,0 +1,9 @@
+//
+//  AutomaticVehicleIdResponse.swift
+//  ShortTrips
+//
+//  Created by Joshua Adams on 9/18/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+import Foundation
---
+++ ShortTrips/LotStatus.swift
@@ -0,0 +1,9 @@
+//
+//  LotStatus.swift
+//  ShortTrips
+//
+//  Created by Joshua Adams on 9/18/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+import Foundation
