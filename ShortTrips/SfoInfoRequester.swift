10ea80972dc3510f1160ea8f4a41fc5ea3d989c9
devmonster423 <devmonster423@gmail.com>
2015-09-21 16:10:57 -0700

    abstract urls into separate struct

--- ShortTrips/SfoInfoRequester.swift
+++ ShortTrips/SfoInfoRequester.swift
@@ -24,96 +24,83 @@ typealias GeofenceClosure = (Geofence?, ErrorType?) -> Void
 typealias DriverResponseClosure = (DriverResponse?, ErrorType?) -> Void
 
 class SfoInfoRequester {
-  private static let baseUrl = "https://216.9.96.29:9000/taxiws/services/taxi/"
-  private static let lotStatusUrl = "lot_status"
-  private static let terminalSummaryUrl = "flight/arrival/summary"
-  private static let flightsForTerminalUrl = "flight/arrival/details"
-  private static let aviUrl = "device/avi/"
-  private static let antennaUrl = "device/avi/transponder/"
-  private static let allCidsUrl = "device/cid/"
-  private static let cidForSmartCardUrl = "device/cid/smart_card/"
-  private static let mobileStateUrl = "device/mobile/state"
-  private static let referenceConfigUrl = "reference/config"
-  private static let geofenceUrl = "geofence"
-  private static let locationUrl = "location"
-  private static let driverLoginUrl = "driver/login"
   
   class func requestLotStatus(response: LotStatusClosure) {
-    Alamofire.request(.GET, baseUrl + lotStatusUrl, parameters: nil).responseObject { (statusResponse: LotStatusResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.queueStatus, parameters: nil).responseObject { (statusResponse: LotStatusResponse?, error: ErrorType?) in
       response(statusResponse?.lotStatus, error)
     }
   }
 
   class func requestFlightsForTerminal(terminal: Int, hour: Int, response: FlightsForTerminalClosure) {
     let params = ["terminal_id": terminal, "hour": hour]
-    Alamofire.request(.GET, baseUrl + flightsForTerminalUrl, parameters: params).responseObject { (flightsForTerminalResponse: FlightsForTerminalResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Flight.Arrival.details, parameters: params).responseObject { (flightsForTerminalResponse: FlightsForTerminalResponse?, error: ErrorType?) in
       response(flightsForTerminalResponse?.flightDetailResponse?.flightDetails, error)
     }
   }
   
   class func requestTerminalSummary(hour: Int, response: TerminalSummaryClosure) {
     let params = ["hour": hour]
-    Alamofire.request(.GET, baseUrl + terminalSummaryUrl, parameters: params).responseObject { (terminalSummaryResponse: TerminalSummaryResponse?, error: ErrorType?) in response(terminalSummaryResponse?.terminalSummaryArrivalsResponse?.terminalSummaryArrivalsListResponse?.terminalSummaryArrivalsListListResponse?.terminalSummaryArrivalsList, error)
+    Alamofire.request(.GET, Url.Flight.Arrival.summary, parameters: params).responseObject { (terminalSummaryResponse: TerminalSummaryResponse?, error: ErrorType?) in response(terminalSummaryResponse?.terminalSummaryArrivalsResponse?.terminalSummaryArrivalsListResponse?.terminalSummaryArrivalsListListResponse?.terminalSummaryArrivalsList, error)
     }
   }
   
   class func requestAutomaticVehicleIds(response: AviClosure) {
-    Alamofire.request(.GET, baseUrl + aviUrl, parameters: nil).responseObject { (aviResponse: AutomaticVehicleIdResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Device.Avi.avi, parameters: nil).responseObject { (aviResponse: AutomaticVehicleIdResponse?, error: ErrorType?) in
       response(aviResponse?.automaticVehicleIds, error)
     }
   }
 
   class func requestAntenna(transponderId: Int, response: AntennaClosure) {
-    Alamofire.request(.GET, baseUrl + antennaUrl + "\(transponderId)", parameters: nil).responseObject { (antennaResponse: AntennaResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Device.Avi.transponder(transponderId), parameters: nil).responseObject { (antennaResponse: AntennaResponse?, error: ErrorType?) in
       response(antennaResponse?.antenna, error)
     }
   }
   
   class func requestAllCids(response: AllCidsClosure) {
-    Alamofire.request(.GET, baseUrl + allCidsUrl, parameters: nil).responseObject { (allCidsResponse: AllCidsResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Device.Cid.cid, parameters: nil).responseObject { (allCidsResponse: AllCidsResponse?, error: ErrorType?) in
       response(allCidsResponse?.cidListResponse?.cidList, error)
     }
   }
   
   class func requestCidForSmartCard(smartCardId: Int, response: CidForSmartCardClosure) {
-    Alamofire.request(.GET, baseUrl + cidForSmartCardUrl + "\(smartCardId)", parameters: nil).responseObject { (cidResponse: CidResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Device.Cid.smartCard(smartCardId), parameters: nil).responseObject { (cidResponse: CidResponse?, error: ErrorType?) in
       response(cidResponse?.cid, error)
     }
   }
   
   class func requestReferenceConfig(response: ReferenceConfigClosure) {
-    Alamofire.request(.GET, baseUrl + referenceConfigUrl, parameters: nil).responseObject { (referenceConfigResponse: ReferenceConfigResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Reference.config, parameters: nil).responseObject { (referenceConfigResponse: ReferenceConfigResponse?, error: ErrorType?) in
       response(referenceConfigResponse?.referenceConfig, error)
     }
   }
   
   class func requestAllGeofences(response: AllGeofencesClosure) {
-    Alamofire.request(.GET, baseUrl + geofenceUrl, parameters: nil).responseObject { (allGeofencesResponse: AllGeofencesResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Geofence.geofence, parameters: nil).responseObject { (allGeofencesResponse: AllGeofencesResponse?, error: ErrorType?) in
       response(allGeofencesResponse?.geofenceListResponse?.geofenceList, error)
     }
   }
   
   class func requestGeofenceForLocation(longitude: Float, latitude: Float, buffer: Float, response: GeofenceClosure) {
     let params = ["longitude": longitude, "latitude": latitude, "buffer": buffer]
-    Alamofire.request(.GET, baseUrl + geofenceUrl + "/" + locationUrl, parameters: params).responseObject { (geofenceResponse: GeofenceResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Geofence.location, parameters: params).responseObject { (geofenceResponse: GeofenceResponse?, error: ErrorType?) in
       response(geofenceResponse?.geofence, error)
     }
   }
   
   class func requestGeofenceForId(id: Int, response: GeofenceClosure) {
-    Alamofire.request(.GET, baseUrl + geofenceUrl + "/" + "\(id)", parameters: nil).responseObject { (geofenceResponse: GeofenceResponse?, error: ErrorType?) in
+    Alamofire.request(.GET, Url.Geofence.singleGeofence(id), parameters: nil).responseObject { (geofenceResponse: GeofenceResponse?, error: ErrorType?) in
       response(geofenceResponse?.geofence, error)
     }
   }
 
   class func authenticateDriver(credential: Credential) {
-    Alamofire.request(.POST, baseUrl + driverLoginUrl, parameters: Mapper().toJSON(credential), encoding: .JSON).response { _, _, _, error in
+    Alamofire.request(.POST, Url.Driver.login, parameters: Mapper().toJSON(credential), encoding: .JSON).response { _, _, _, error in
       print(error)
     }
   }
   
   class func postMobileStateChanges(mobileStateChange: MobileStateChange) {
-    Alamofire.request(.POST, baseUrl + mobileStateUrl, parameters: Mapper().toJSON(mobileStateChange), encoding: .JSON).response { _, _, _, error in
+    Alamofire.request(.POST, Url.Device.mobileState, parameters: Mapper().toJSON(mobileStateChange), encoding: .JSON).response { _, _, _, error in
       print(error)
     }
   }
---
+++ ShortTrips/Url.swift
@@ -0,0 +1,106 @@
+//
+//  URL.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 9/21/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+struct Url {
+  
+  private static let base = "https://216.9.96.29:9000/taxiws/services/taxi/"
+  
+  static let queueStatus = base + "status/queue"
+  
+  struct Device {
+    private static let device = base + "device/"
+    
+    static let mobileState = device + "mobile/state"
+    
+    struct Avi {
+      static let avi = device + "avi"
+      
+      static func transponder(transponderId: Int) -> String {
+        return avi + "/transponder/\(transponderId)"
+      }
+    }
+    
+    struct Cid {
+      static let cid = device + "cid"
+      
+      static func smartCard(smartCardId: Int) -> String {
+        return cid + "/smart_card/\(smartCardId)"
+      }
+      static func singleCid(cidId: Int) -> String {
+        return cid + "/\(cidId)"
+      }
+    }
+  }
+  
+  struct Dispatcher {
+    private static let dispatcher = base + "dispatcher/"
+    
+    static let holdingLotCapacity = dispatcher + "holdinglot/capacity"
+  }
+  
+  struct Driver {
+    private static let driver = base + "driver/"
+    
+    static let login = driver + "login"
+  }
+  
+  struct Flight {
+    private static let flight = base + "flight/"
+    
+    static let summary = flight + "summary"
+    
+    struct Arrival {
+      private static let arrival = flight + "arrival/"
+      
+      static let summary = arrival + "summary"
+      static let details = arrival + "details"
+    }
+    
+    struct Departure {
+      private static let departure = flight + "departure/"
+      
+      static let summary = departure + "summary"
+      static let details = departure + "details"
+    }
+  }
+  
+  struct Geofence {
+    static let geofence = base + "geofence/"
+    
+    static let location = geofence + "location"
+    static func singleGeofence(geofenceId: Int) -> String {
+      return geofence + "\(geofenceId)"
+    }
+  }
+  
+  struct Reference {
+    private static let reference = base + "reference/"
+    
+    static let config = reference + "config"
+    static let state = reference + "state"
+  }
+  
+  struct Trip {
+    private static let trip = base + "trip/"
+    
+    static let start = trip + "start"
+    static let status = trip + "status"
+    static func end(tripId: Int) -> String {
+      return trip + "\(tripId)/end"
+    }
+    static func invalidGeofenceDetails(tripId: Int) -> String {
+      return trip + "\(tripId)/invalid_geofence_details"
+    }
+    static func invalidate(tripId: Int) -> String {
+      return trip + "\(tripId)/invalidate"
+    }
+    static func ping(tripId: Int) -> String {
+      return trip + "\(tripId)/ping"
+    }
+  }
+}
