8100798b8e0e4f4f43754d97a3cdde072808b197
devmonster423 <devmonster423@gmail.com>
2015-09-23 16:56:13 -0700

    imageclient

--- ShortTrips/ApiClient.swift
+++ ShortTrips/ApiClient.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Alamofire
 
-struct ApiClient: DeviceClient, DispatcherClient, DriverClient, FlightClient, GeofenceClient, ReferenceClient {
+struct ApiClient: DeviceClient, DispatcherClient, DriverClient, FlightClient, GeofenceClient, ImageClient, ReferenceClient {
   
   private static let sfoUsername = "taxi_short@staging"
   private static let sfoPassword = "mvUh6tYEwU9nYDrQ"
