fc5d78b713c588cdb478d8e2737e6230b41e5abc
devmonster423 <devmonster423@gmail.com>
2015-09-25 14:07:55 -0700

    test airline client

---
+++ ShortTripsTests/AirlineClientSpec.swift
@@ -0,0 +1,33 @@
+//
+//  ApiClientSpec.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 9/25/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+@testable import ShortTrips
+import Quick
+import Nimble
+import PivotalCoreKit
+
+class AirlineClientSpec: QuickSpec {
+  
+  override func spec() {
+    
+    describe("the airline client") {
+      it("can request codes") {
+        
+        waitUntil(timeout: 60) { done in
+          
+          ApiClient.codes({ airlines, error in
+            
+            expect(airlines).toNot(beNil())
+            done()
+          })
+        }
+      }
+    }
+  }
+}
+
