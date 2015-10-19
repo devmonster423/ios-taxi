988246f44e81d9b628f427c89fb1110086c31a4a
devmonster423 <devmonster423@gmail.com>
2015-10-19 14:09:56 -0700

    start of geofence client tests

---
+++ ShortTripsTests/AllGeofenceMock.swift
@@ -0,0 +1,27 @@
+//
+//  AllGeofenceMock.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 10/19/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+import Foundation
+
+var AllGeofenceMock =
+[
+  "response":
+  [
+    "geofence_list":
+    [
+      [
+        "category": "City",
+        "description": "City",
+        "geofence_id": 16,
+        "name": "Colma"
+      ]
+    ]
+  ]
+]
+
+// {"response":{"geofence_list":[{"category":"City","description":"City","geofence_id":16,"name":"Colma"},{"category":"Location","description":"Location","geofence_id":12,"name":"SFO Taxi Entry Gate"},{"category":"Location","description":"Location","geofence_id":11,"name":"SFO Taxi Domestic Exit"},{"category":"City","description":"City","geofence_id":5,"name":"Millbrae"},{"category":"City","description":"City","geofence_id":4,"name":"Hillsborough"},{"category":"City","description":"City","geofence_id":2,"name":"Burlingame"},{"category":"City","description":"City","geofence_id":15,"name":"Open Space"},{"category":"City","description":"City","geofence_id":7,"name":"Unincorp"},{"category":"Place","description":"Place","geofence_id":10,"name":"San Francisco International Airport"},{"category":"City","description":"City","geofence_id":3,"name":"Foster City"},{"category":"City","description":"City","geofence_id":17,"name":"Daly City"},{"category":"City","description":"City","geofence_id":8,"name":"San Mateo"},{"category":"City","description":"City","geofence_id":1,"name":"Brisbane"},{"category":"Location","description":"Location","geofence_id":13,"name":"SFO International Exit"},{"category":"City","description":"City","geofence_id":6,"name":"San Bruno"},{"category":"City","description":"City","geofence_id":9,"name":"South San Francisco"}]}}
---
+++ ShortTripsTests/SingleGeofenceMock.swift
@@ -0,0 +1,23 @@
+//
+//  SingleGeofenceMock.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 10/19/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+import Foundation
+
+var SingleGeofenceMock =
+
+[
+  "response":
+  [
+    "id": 10,
+    "name": "San Francisco International Airport",
+    "geofence_status": true
+  ]
+]
+
+
+// {"response":{"id":10,"name":"San Francisco International Airport","geofence_status":true}}
