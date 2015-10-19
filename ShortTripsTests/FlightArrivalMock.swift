a8fb65ecbe9d57b6ea99d95460b7d40ccbd05f4a
devmonster423 <devmonster423@gmail.com>
2015-10-19 13:40:15 -0700

    test mocks for flight info

---
+++ ShortTripsTests/FlightArrivalMock.swift
@@ -0,0 +1,29 @@
+//
+//  FlightArrivalMock.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 10/19/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+import Foundation
+
+var FlightArrivalMock =
+[
+  "response":
+    [
+      "details":
+        [
+          [
+            "airline_name": "Delta",
+            "bags": "0",
+            "estimated_time": "2:09 PM",
+            "flight_number": "DL2621",
+            "remarks": "",
+            "schedule_time": "2:30 PM"
+          ]
+      ]
+  ]
+]
+
+// {"response":{"details":[{"airline_name":"Delta","bags":0,"estimated_time":"2:09 PM","flight_number":"DL2621","remarks":"","scheduled_time":"2:30 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"1:30 PM","flight_number":"WN398","remarks":"On Time","scheduled_time":"2:00 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"2:05 PM","flight_number":"WN2558","remarks":"On Time","scheduled_time":"2:00 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"1:55 PM","flight_number":"WN2608","remarks":"Cancelled","scheduled_time":"1:55 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"1:50 PM","flight_number":"WN2129","remarks":"On Time","scheduled_time":"1:50 PM"},{"airline_name":"Delta","bags":0,"estimated_time":"1:27 PM","flight_number":"DL1151","remarks":"","scheduled_time":"1:47 PM"},{"airline_name":"Delta","bags":0,"estimated_time":"1:49 PM","flight_number":"DL3222","remarks":"","scheduled_time":"1:30 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"1:35 PM","flight_number":"WN369","remarks":"On Time","scheduled_time":"12:35 PM"},{"airline_name":"Southwest Airlines","bags":0,"estimated_time":"1:30 PM","flight_number":"WN210","remarks":"In Range","scheduled_time":"12:35 PM"}],"terminal_id":1}}
---
+++ ShortTripsTests/FlightClientSpec.swift
@@ -0,0 +1,40 @@
+//
+//  FlightClientSpec.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 10/19/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+@testable import ShortTrips
+import Quick
+import Nimble
+import Mockingjay
+
+class FlightClientSpec: QuickSpec {
+  
+  override func spec() {
+    
+    describe("the flight client") {
+      it("can request flight details") {
+        
+        self.stub(uri(Url.Flight.Arrival.details), builder: json(FlightArrivalMock))
+        
+        ApiClient.requestFlightsForTerminal(1, hour: 1) { flights in
+          
+          expect(flights).toNot(beNil())
+        }
+      }
+      
+      it("can request terminal summary") {
+        
+        self.stub(uri(Url.Flight.Arrival.summary), builder: json(TerminalSummaryMock))
+        
+        ApiClient.requestTerminalSummary(1) { terminalSummaries, hour, error in
+          
+          expect(terminalSummaries).toNot(beNil())
+        }
+      }
+    }
+  }
+}
