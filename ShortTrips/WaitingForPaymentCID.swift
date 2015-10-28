57f46b1fcd2c7c646547717de2f8a1329028d51c
devmonster423 <devmonster423@gmail.com>
2015-10-27 17:26:36 -0700

    [#105264198] Add poller to WaitingForPaymentCID Event

--- ShortTrips/WaitingForPaymentCID.swift
+++ ShortTrips/WaitingForPaymentCID.swift
@@ -14,13 +14,41 @@ struct WaitingForPaymentCID {
   let stateName = "waitingForPaymentCID"
   static let sharedInstance = WaitingForPaymentCID()
 
+  private var poller: Poller?
   private var state: TKState
 
   private init() {
     state = TKState(name: stateName)
+    
+    state.setDidEnterStateBlock { _, _ in
+      
+      self.poller = Poller.init(timeout: 60, action: { _ in
+        if let driver = DriverManager.sharedInstance.getCurrentDriver() {
+          ApiClient.requestCidForSmartCard(driver.cardId) { cid in
+            
+            // TODO: actually verify if the smart card request succedeed
+            //        if let cid = cid where
+            //        cid.cidLocation == "entry" {
+            
+            LatestCidIsPaymentCid.sharedInstance.fire()
+
+            //  }
+          }
+        }
+      })
+    }
+    
+    state.setDidExitStateBlock { _, _ in
+      self.poller?.stop()
+    }
   }
   
   func getState() -> TKState {
     return state
   }
 }
+
+
+
+
+
