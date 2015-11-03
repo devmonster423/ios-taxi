a4f5c4ddce43a22b76ba13e7e6cdb7f29fa3817b
devmonster423 <devmonster423@gmail.com>
2015-11-03 15:07:41 -0800

    add scenario 7

---
+++ ShortTripsTests/TripScenario7Spec.swift
@@ -0,0 +1,57 @@
+//
+//  TripScenario7Spec.swift
+//  ShortTrips
+//
+//  Created by Joshua Adams on 11/3/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+@testable import ShortTrips
+import Quick
+import Nimble
+
+class TripScenario7Spec: QuickSpec {
+  
+  override func spec() {
+    
+    describe("the trip manager") {
+      it("can handle scenario 1") {
+        let machine = StateManager.sharedInstance.getMachine()
+        
+        // can be initialized
+        expect(machine).toNot(beNil())
+        
+        // can fire DriverDispatched and make correct state change
+        InsideTaxiLoopExit.sharedInstance.fire()
+        expect(machine.isInState(WaitingForPaymentCID.sharedInstance.getState())).to(beTrue())
+        
+        LatestCidIsPaymentCid.sharedInstance.fire()
+        expect(machine.isInState(VerifyingTaxiLoopAvi.sharedInstance.getState())).to(beTrue())
+        
+        LatestAviReadAtTaxiLoop.sharedInstance.fire()
+        expect(machine.isInState(Ready.sharedInstance.getState())).to(beTrue())
+        
+        // can fire DriverExitsSfo and make correct state change
+        OutsideSfo.sharedInstance.fire()
+        expect(machine.isInState(VerifyingExitAvi.sharedInstance.getState())).to(beTrue())
+        
+        LatestAviReadAtExit.sharedInstance.fire()
+        expect(machine.isInState(WaitingForStartTrip.sharedInstance.getState())).to(beTrue())
+        
+        TripStarted.sharedInstance.fire()
+        expect(machine.isInState(InProgress.sharedInstance.getState())).to(beTrue())
+        
+        // can fire DriverReturnsToSfo and make correct state change
+        InsideSfo.sharedInstance.fire()
+        expect(machine.isInState(VerifyingInboundAvi.sharedInstance.getState())).to(beTrue())
+        
+        LatestAviReadInbound.sharedInstance.fire()
+        expect(machine.isInState(ValidatingTrip.sharedInstance.getState())).to(beTrue())
+        
+        // can fire TripValidated and make correct state change
+        TripValidated.sharedInstance.fire()
+        expect(machine.isInState(NotReady.sharedInstance.getState())).to(beTrue())
+      }
+    }
+  }
+}
