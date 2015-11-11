0c6dfa8b1ea618e5e08a715452492c6642b7fb89
devmonster423 <devmonster423@gmail.com>
2015-11-11 09:08:00 -0800

    remove wrapper struct from FlightDetails model

--- ShortTrips/FlightDetailsWrapper.swift
+++ ShortTrips/FlightDetailsWrapper.swift
@@ -1,21 +1,21 @@
 //
-//  FlightDetailResponse.swift
+//  FlightDetailsWrapper.swift
 //  ShortTrips
 //
-//  Created by Joshua Adams on 9/17/15.
+//  Created by Josh Adams and 🐈 on 9/17/15.
 //  Copyright © 2015 SFO. All rights reserved.
 //
 
 import ObjectMapper
 
-struct FlightDetailResponse: Mappable {
+struct FlightDetailsWrapper: Mappable {
   var flightDetails: [Flight]!
   var terminalId: TerminalId!
   
   init?(_ map: Map){}
   
   mutating func mapping(map: Map) {
-    flightDetails <- map["details"]
-    terminalId <- map["terminal_id"]
+    flightDetails <- map["response.details"]
+    terminalId <- map["response.terminal_id"]
   }
 }
---
+++ ShortTripsTests/FlightDetailsWrapperSpec.swift
@@ -0,0 +1,39 @@
+//
+//  FlightDetailsWrapperSpec.swift
+//  ShortTrips
+//
+//  Created by Joshua Adams on 9/21/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+@testable import ShortTrips
+import Quick
+import Nimble
+import Foundation
+import ObjectMapper
+
+class FlightDetailResponseSpec: QuickSpec {
+  var flightDetailsWrapper: FlightDetailsWrapper!
+  var flightDetails: [Flight]!
+  
+  override func spec() {
+    describe("the FlightDetailsWrapper") {
+      beforeEach {
+        self.flightDetailsWrapper = Mapper<FlightDetailsWrapper>().map(MockFlightDetailsWrapperString)
+      }
+      
+      it("is non-nil") {
+        expect(self.flightDetailsWrapper).toNot(beNil())
+      }
+      
+      it("has a flightDetails") {
+        expect(self.flightDetailsWrapper.flightDetails).toNot(beNil())
+      }
+      
+      it("has a Flight") {
+        expect(self.flightDetailsWrapper.flightDetails[0]).toNot(beNil())
+      }
+    }
+  }
+}
+
