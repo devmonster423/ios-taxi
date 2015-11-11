93c8458cdb61c4e2466dc6b995daa6e95411f832
devmonster423 <devmonster423@gmail.com>
2015-11-11 11:16:03 -0800

    remove wrapper struct from Cid model

--- ShortTrips/CidListWrapper.swift
+++ ShortTrips/CidListWrapper.swift
@@ -1,5 +1,5 @@
 //
-//  CidResponse.swift
+//  CidListWrapper.swift
 //  ShortTrips
 //
 //  Created by Joshua Adams on 9/15/15.
@@ -9,12 +9,16 @@
 import Foundation
 import ObjectMapper
 
-struct CidResponse: Mappable {
-  var cid: Cid!
+struct CidListWrapper: Mappable {
+  var cidList: [Cid]!
+  
+  init(cidList: [Cid]) {
+    self.cidList = cidList
+  }
   
   init?(_ map: Map){}
   
   mutating func mapping(map: Map) {
-    cid <- map["response"]
+    cidList <- map["response.list"]
   }
 }
