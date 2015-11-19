af477d5596fffa02357d8b2c00446ef4a98b3e74
devmonster423 <devmonster423@gmail.com>
2015-11-18 16:41:38 -0800

    update invalidation options

--- ShortTrips/AppQuit.swift
+++ ShortTrips/AppQuit.swift
@@ -31,7 +31,7 @@ extension AppQuit: Event {
 extension AppQuit: Observable {
   func eventIsFiring(info: Any?) {
     if let tripId = TripManager.sharedInstance.getTripId() {
-      ApiClient.invalidate(tripId, validation: ValidationStepWrapper(validationStep: .InvalidGeofence))
+      ApiClient.invalidate(tripId, validation: ValidationStepWrapper(validationStep: .AppQuit))
     }
   }
 }
--- ShortTrips/ValidationStep.swift
+++ ShortTrips/ValidationStep.swift
@@ -10,24 +10,50 @@ import Foundation
 import ObjectMapper
 
 enum ValidationStep: Int {
-  case VehicleMismatch = 2
-  case InvalidGeofence = 5
+  case Duration = 1
+  case Vehicle = 2
+  case DriverCardId = 3
+  case MacAddress = 4
+  case Geofence = 5
+  case GpsFailure = 6
+  case NetworkFailure = 7
+  case UserLogout = 8
+  case AppQuit = 9
+  case AppCrash = 10
   
-  func description() -> String {
+  func description() -> String? {
     switch self {
-    case .VehicleMismatch:
+    case .Vehicle:
       return "Vehicle miss match found (1404,null)"
-    case .InvalidGeofence:
+    case .Geofence:
       return "invalid geofence"
+    default:
+      return nil
     }
   }
   
   func name() -> String {
     switch self {
-    case .VehicleMismatch:
-      return "vehicle"
-    case .InvalidGeofence:
-      return "geofence"
+    case .Duration:
+      return "validate_trip_duration"
+    case .Vehicle:
+      return "validate_vehicle"
+    case .DriverCardId:
+      return "validate_driver_card_id"
+    case .MacAddress:
+      return "validate_mac_address"
+    case .Geofence:
+      return "validate_geofence"
+    case .GpsFailure:
+      return "gps_failure_exception"
+    case .NetworkFailure:
+      return "network_failure_exception"
+    case .UserLogout:
+      return "user_logout_exception"
+    case .AppQuit:
+      return "app_quit_exception"
+    case .AppCrash:
+      return "app_crash_exception"
     }
   }
 }
