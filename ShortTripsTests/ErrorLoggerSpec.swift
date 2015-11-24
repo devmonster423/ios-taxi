260cdec585ffc8038e4c68d8ce257c24e8ba0f1e
devmonster423 <devmonster423@gmail.com>
2015-11-23 16:36:14 -0800

    test error logger

---
+++ ShortTripsTests/ErrorLoggerSpec.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorLogger.swift
+//  ShortTrips
+//
+//  Created by Matt Luedke on 11/23/15.
+//  Copyright © 2015 SFO. All rights reserved.
+//
+
+@testable import ShortTrips
+import Quick
+import Nimble
+import Foundation
+
+class ErrorLoggerSpec: QuickSpec {
+  
+  override func spec() {
+    describe("the ErrorLogger") {
+      
+      it("can log a request") {
+        ErrorLogger.log(NSURLRequest(URL: NSURL(string: "http://www.google.com")!), error: NSError(domain: "", code: 500, userInfo: nil))
+      }
+    }
+  }
+}
