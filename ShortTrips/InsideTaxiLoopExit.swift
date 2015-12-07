34f92db541fab84c41b16a868d845d97becd46ec
devmonster423 <devmonster423@gmail.com>
2015-12-07 14:16:58 -0800

    skip optional entry steps when appropriate

--- ShortTrips/InsideTaxiLoopExit.swift
+++ ShortTrips/InsideTaxiLoopExit.swift
@@ -18,11 +18,20 @@ struct InsideTaxiLoopExit {
   private init() {
     
     let insideTaxiLoopExitEvent = TKEvent(name: eventNames[0],
-      transitioningFromStates: [WaitingInHoldingLot.sharedInstance.getState(), NotReady.sharedInstance.getState()],
+      transitioningFromStates: [WaitingInHoldingLot.sharedInstance.getState(), NotReady.sharedInstance.getState(), WaitingForEntryCid.sharedInstance.getState(), AssociatingDriverAndVehicleAtEntry.sharedInstance.getState(), VerifyingEntryGateAvi.sharedInstance.getState()],
       toState: WaitingForPaymentCid.sharedInstance.getState())
     
-    insideTaxiLoopExitEvent.setShouldFireEventBlock { _, _ -> Bool in
-      return LocationManager.locationActiveAndAuthorized()
+    insideTaxiLoopExitEvent.setShouldFireEventBlock { _, transition -> Bool in
+      
+      if transition.sourceState == WaitingForEntryCid.sharedInstance.getState()
+        || transition.sourceState == AssociatingDriverAndVehicleAtEntry.sharedInstance.getState()
+        || transition.sourceState == VerifyingEntryGateAvi.sharedInstance.getState() {
+        
+          return TripManager.sharedInstance.getTripId() == nil
+            && LocationManager.locationActiveAndAuthorized()
+      } else {
+        return LocationManager.locationActiveAndAuthorized()
+      }
     }
     
     events = [insideTaxiLoopExitEvent]
