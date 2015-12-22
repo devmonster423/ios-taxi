67d7ce991b02058db18b29188570ee675e230639
devmonster423 <devmonster423@gmail.com>
2015-12-22 11:06:14 -0500

    test taxi wait zone

--- ShortTripsTests/TaxiWaitZoneGeofenceSpec.swift
+++ ShortTripsTests/TaxiWaitZoneGeofenceSpec.swift
@@ -18,7 +18,21 @@ class TaxiWaitZoneGeofenceSpec: QuickSpec {
     describe("the taxi wait zone geofence") {
       
       it("can parse") {
-        expect(LocalGeofence("Taxi_Waiting_Zone")).toNot(beNil())
+        expect(taxiWaitingZone).toNot(beNil())
+      }
+      
+      it("denotes a bad point as being outside the geofence") {
+        
+        let badPoint = CLLocationCoordinate2D(latitude:  37.614938, longitude: -122.386390)
+        
+        expect(GeofenceArbiter.checkLocation(badPoint, againstFeatures: taxiWaitingZone.features)).to(beFalse())
+      }
+      
+      it("denotes a good point as being inside the geofence") {
+        
+        let goodPoint = CLLocationCoordinate2D(latitude: 37.616307, longitude: -122.386158)
+        
+        expect(GeofenceArbiter.checkLocation(goodPoint, againstFeatures: taxiWaitingZone.features)).to(beTrue())
       }
     }
   }
