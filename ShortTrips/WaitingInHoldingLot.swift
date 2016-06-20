96f3c46980630ff47df372ddfc946ecbf0757f99
devmonster423 <devmonster423@gmail.com>
2016-06-20 15:19:59 -0700

    automatically jump into payment cid check after either 1 or 10 min

--- ShortTrips/WaitingInHoldingLot.swift
+++ ShortTrips/WaitingInHoldingLot.swift
@@ -19,8 +19,18 @@ struct WaitingInHoldingLot {
   private init() {
     state = TKState(name: stateName)
     
-    state.setDidEnterStateBlock { _, _  in
+    state.setDidEnterStateBlock { _, transition  in
       postNotification(SfoNotification.State.update, value: self.getState())
+      
+      let intervalSec: Double = TripValidated.sharedInstance.getEvents().contains(transition.event)
+        ? 60
+        : 60 * 10
+      
+      let interval = dispatch_time(DISPATCH_TIME_NOW, Int64(intervalSec * Double(NSEC_PER_SEC)))
+      
+      dispatch_after(interval, dispatch_get_main_queue()) {
+        InsideTaxiLoopExit.sharedInstance.fire()
+      }
     }
   }
   
