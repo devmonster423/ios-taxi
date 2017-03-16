8f3c8e008bf9424d0cff8a7891e6d866e4fc2e6b
devmonster423 <devmonster423@gmail.com>
2017-03-16 14:51:24 -0700

    add more tests for new geofence sections

--- ShortTripsTests/TaxiMergedGeofenceSpec.swift
+++ ShortTripsTests/TaxiMergedGeofenceSpec.swift
@@ -49,6 +49,62 @@ class TaxiMergedGeofenceSpec: QuickSpec {
         
         expect(GeofenceArbiter.checkLocation(sanMateoPoint)).to(beTrue())
       }
+      
+      it("finds a good point in Belmont inside the geofence") {
+        
+        let sanMateoPoint = CLLocationCoordinate2D(latitude: 37.517546, longitude: -122.286308)
+        
+        expect(GeofenceArbiter.checkLocation(sanMateoPoint)).to(beTrue())
+      }
+      
+      it("finds a bad point in Belmont outside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.505563, longitude: -122.289612)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beFalse())
+      }
+      
+      it("finds a good point at Skyline College inside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.629892, longitude: -122.467411)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beTrue())
+      }
+      
+      it("finds a bad point in Pacifica outside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.638728, longitude: -122.487710)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beFalse())
+      }
+      
+      it("finds a good point at Geneva and Mission inside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.716413, longitude: -122.441007)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beTrue())
+      }
+      
+      it("finds a bad point in City College of San Francisco outside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.725205, longitude: -122.452337)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beFalse())
+      }
+      
+      it("finds a good point at Candlestick Cove inside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.709029, longitude: -122.393607)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beTrue())
+      }
+      
+      it("finds a bad point on 101 outside the geofence") {
+        
+        let point = CLLocationCoordinate2D(latitude: 37.716277, longitude: -122.398499)
+        
+        expect(GeofenceArbiter.checkLocation(point)).to(beFalse())
+      }
     }
   }
 }
